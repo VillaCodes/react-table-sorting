e4b8428ccb5e99a4789656e274c5aef9a1e618b7
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-02-08 23:31:08 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
 
+const fetchData = () => {
+  return axios.get("https://randomuser.me/api/?results=20").then((data) => console.log(data)).catch((error) => console.error(error))
+};
+
+//https://randomuser.me/api/?results=20
 function App() {
+
+  useEffect (() => {
+   fetchData();
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Sandbox!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's make some magic happen!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
