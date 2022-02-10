7f8ed85694882bdf51a4928a833be34b9b8e5611
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-02-09 22:44:57 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import axios from "axios";
 import {useEffect, useState} from "react";
 
+
+//the function below would preferably be inside of a util/api folder+file
 const fetchData = () => {
   return axios.get("https://randomuser.me/api/?results=20").then((response) => {
     console.log(response)
@@ -14,12 +16,26 @@ const fetchData = () => {
 function App() {
 
   const [users, setUsers] = useState([]);
+  const [locations, setLocations] = useState([]);
 
   useEffect (() => {
-   fetchData().then(setUsers);
-  }, [])
+   fetchData().then((people) => {
+     setUsers(people);
+     //the map below could also employ destructuring. people.map(({location}) => location)
+     setLocations(people.map((person) => person.location))
+   });
+  }, []);
 
- 
+  console.log(locations)
+
+  // const table = users.map((user, userId) => {
+  //   return (
+  //   <tb>
+  //   <tr>
+  //     <th>{user.location}</th>
+  //   </tr>
+  //   </tb>
+  // )})
   return (
     <div className="App">
       <header className="App-header">
